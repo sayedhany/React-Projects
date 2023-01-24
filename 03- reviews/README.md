61bb11fbc0ac209494c5238d9003a4eacfdd6ed5
sayedhany <sayed591999@gmail.com>
2023-01-24 15:46:55 +0200

    add review project (small)

---
+++ 03- reviews/README.md
@@ -0,0 +1,4 @@
+## Review program.
+
+- Live View: https://reviewtest.netlify.app
+- ![image of site](https://i.ibb.co/WHgpzFN/review-site.png)
---
+++ 03- reviews/src/Review.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button
+          className="prev-btn"
+          onClick={() =>
+            setIndex((index) => {
+              console.log(index);
+              if (index > 0) {
+                return --index;
+              } else {
+                index = 0;
+                return index;
+              }
+            })
+          }
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className="next-btn"
+          onClick={() =>
+            setIndex((index) => {
+              console.log(index);
+              if (index < people.length - 1) {
+                return ++index;
+              } else {
+                index = people.length - 1;
+                return index;
+              }
+            })
+          }
+        >
+          <FaChevronRight />
+        </button>
+      </div>
+      <button
+        className="random-btn"
+        onClick={() => {
+          const random = Math.floor(Math.random() * people.length);
+          setIndex(random);
+        }}
+      >
+        Surprise Me
+      </button>
+    </article>
+  );
+};
+
+export default Review;
