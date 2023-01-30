6b52b9c673650555580686f13a5a08b0e8049faa
sayedhany <sayedh@mail.ru>
2023-01-30 17:37:11 +0200

    add menu project

--- 05- Menu/src/App.js
+++ 05- Menu/src/App.js
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allCategories);
+  function filterItems(category) {
+    const newItems = items.filter((item) => item.category === category);
+    if (category === "all") {
+      setMenuItems(items);
+    } else {
+      setMenuItems(newItems);
+    }
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
